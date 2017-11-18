1d3a94db6349cd657cb1fd3eb176c2d75a25b4c5
hamid eslami <eslami.hamidpp@gmail.com>
2017-11-19 01:11:40 +0330

    Create a translation website

---
+++ index.php
@@ -0,0 +1,105 @@
+<?php include 'layouts/header.php'; ?>
+
+
+    <div class="slider">
+
+        <img src="images/top-arrow.png"  id="top-arrow" alt="">
+        <img src="images/down-arrow.png" id="down-arrow" alt="">
+
+        <span>ترجمانو</span>
+
+        <div id="header-slider">
+
+          <img src="images/banner.jpg" alt="">
+          <img src="images/1.jpg" alt="">
+          <img src="images/2.jpg" alt="">
+          <img src="images/3.jpg" alt="">
+          <img src="images/4.jpg" alt="">
+          <img src="images/5.jpg" alt="">
+          <img src="images/6.jpg" alt="">
+
+
+        </div>
+
+    </div>
+
+    <div class="subs-form">
+
+        <span>ثبت نام در خبرنامه ترجمانو</span>
+        <input type="text" placeholder="نام ...">
+        <input type="text" placeholder="ایمیل ...">
+        <input type="button" value="ثبت نام در خبرنامه">
+
+    </div>
+
+
+
+    <div class="content">
+
+      <hr>
+      <div class="description">
+        <div class="container">
+            <div class="col-lg-4 des">
+              <span><span class="glyphicon glyphicon-king"></span> ترجمه تخصصی</span>
+              <p>لورم ایپسوم یا طرح‌نما (به انگلیسی: Lorem ipsum) به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد.</p>
+            </div>
+
+            <div class="col-lg-4 des">
+              <span><span class="glyphicon glyphicon-education"></span> ترجمه دانشجویی</span>
+              <p>لورم ایپسوم یا طرح‌نما (به انگلیسی: Lorem ipsum) به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد.</p>
+            </div>
+
+            <div class="col-lg-4 des">
+              <span><span class="glyphicon glyphicon-camera"></span> ترجمه کتاب و فیلم</span>
+              <p>لورم ایپسوم یا طرح‌نما (به انگلیسی: Lorem ipsum) به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید، تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد.</p>
+            </div>
+        </div>
+      </div>
+
+      <hr>
+
+      <div class="special-part">
+
+        <span class="special-section">انگلیسی به فارسی</span>
+        <span class="special-section">فارسی به انگلیسی</span>
+        <span class="special-section">فرانسه به فارسی</span>
+        <span class="special-section">فارسی به فرانسه</span>
+        <span class="special-section latest">فرانسه به انگلیسی</span>
+        <span class="special-section latest">انگلیسی به فرانسه</span>
+
+      </div>
+
+      <div class="container">
+
+          <h2 style="margin-top: 30px;">مقالات</h2>
+
+      </div>
+
+      <div class="articles">
+
+          <div class="article">
+              <span>آموزش ترجمه بخش اول</span>
+              <img src="images/article2.png" alt="">
+
+          </div>
+          <div class="article">
+              <span>آموزش ترجمه بخش دوم</span>
+              <img src="images/article2.png" alt="">
+
+          </div>
+          <div class="article">
+              <span>آموزش ترجمه بخش سوم</span>
+              <img src="images/article2.png" alt="">
+
+          </div>
+          <div class="article">
+              <span>آموزش ترجمه بخش چهارم</span>
+              <img src="images/article2.png" alt="">
+
+          </div>
+
+      </div>
+
+    </div>
+
+<?php include 'layouts/footer.php'; ?>
