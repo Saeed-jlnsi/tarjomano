6eb52674e4fa600c4a88d2219635b0a3d41eb9b7
hamideslami <eslami.hamidpp@gmail.com>
2018-01-30 01:09:37 +0330

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,81 @@
-<?php include 'layouts/header.php'; ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Translate</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-rtl.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top top-menu" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/works/translate/">ترجمانو</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">خدمات ترجمه&nbsp;<span class="caret"></span></a>
+
+
+
+                    <ul class="dropdown-menu">
+
+                        <li><a href="#">ترجمه فیلم</a></li>
+                        <hr>
+                        <li><a href="#">ترجمه کتاب</a></li>
+                        <hr>
+                        <li><a href="#">ترجمه تخصصی</a></li>
+
+                    </ul>
+
+                </li>
+                <li>
+                    <a href="#">درباره ما</a>
+                </li>
+                <li>
+                    <a href="#">ارتباط با ما</a>
+                </li>
+
+                <li>
+                    <a href="#">تعرفه ها</a>
+                </li>
+            </ul>
+
+
+                <!-- <div class="nav navbar-left">
+                    <form action="" method="post" style="margin-top: 10px">
+                        <button class="btn btn-danger btn-sm">خروج از حساب کاربری</button>
+                    </form>
+                </div> -->
+
+
+                <!-- <ul class="nav navbar-nav navbar-left">
+                    <li>
+                        <a href="#">ورود</a>
+                    </li>
+                    <li>
+                        <a href="#">ثبت نام</a>
+                    </li>
+                </ul> -->
+
+
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
 
 
     <div class="slider">
